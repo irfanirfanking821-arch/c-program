a4ce1de383aebeb8b697cff3de0898c2f5919ed5
irfanirfanking821-arch <irfanirfanking821@gmail.com>
2025-09-02 11:22:08 +0500

    q10 octal

---
+++ Q10.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int octal,decimal;
+	printf("enter an octal number:");
+	scanf("%0,&octal");
+	decimal=octal;
+	printf("decimal value=%d\n",decimal);
+	return 0;
+}
